8bf86c42d104ffb251aef6d00aa40c42b7bb8370
mikky <mikkydidi34@gmail.com>
2024-05-07 15:19:33 -0400

    add backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Google_logo.svg.png';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleClick = () => {
+        window.open('https://google.ca', '_blank');
+        console.log('clicked');
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" onClick={() => handleClick()}/>
+                <a
+                    className="App-link"
+                    href="https://google.ca"
+                    target={'_parent'}
+                    rel="noopener noreferrer"
+                >
+                    To google search
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
