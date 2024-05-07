6239ba761152e3c32e330df8c1a745b7ac6d25cd
Pat51438 <150244170+Pat51438@users.noreply.github.com>
2024-05-07 14:42:49 -0400

    logo modifier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import logo from './peanut-butter-chocolate-swirl-cookies-2.jpg';
 import './App.css';
+import anotherImage from './logo512.png';
 
 function App() {
+  const [currentImage, setCurrentImage] = useState(logo);
+  const handleClick = () => {
+    setCurrentImage(currentImage === logo ? anotherImage : logo);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img onClick={handleClick} src={currentImage} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://google.ca"
+          target="_parent"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to google
         </a>
       </header>
     </div>
