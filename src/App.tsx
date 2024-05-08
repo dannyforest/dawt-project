d67ed0791490bbced307959fbe33084eca138908
Benaissanabila <benaissa.nabila14@gmail.com>
2024-05-08 10:39:13 -0400

    addes backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import newLogo from './newLogo.svg'
 
 function App() {
+  const [currentLogo, setCurrentLogo] = useState(logo );
+  const handleClick=()=>{
+    setCurrentLogo(currentLogo === logo ? newLogo : logo);}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img  src={currentLogo} className="App-logo" alt="logo"  onClick={handleClick}/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://google.ca"
+          target="_parent"
           rel="noopener noreferrer"
         >
-          Learn React
+          To google search
         </a>
       </header>
     </div>
